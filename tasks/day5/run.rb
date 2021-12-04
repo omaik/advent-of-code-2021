7326bca76b2c0ebaf995fbcfb2bacfa26a781cd5
Oleh Maikovych <18499500+omaik@users.noreply.github.com>
2021-12-04 10:06:26 +0200

    master: day5 placeholder

---
+++ tasks/day5/run.rb
@@ -0,0 +1,17 @@
+module Tasks
+  module Day5
+    class Run
+      def call1
+      end
+
+      def call2
+      end
+
+      private
+
+      def input
+        Input.call
+      end
+    end
+  end
+end
